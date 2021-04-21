7c60dac931fc43e2a6bd93ef50d6271cc80d8cf8
Andrew Defante <andrew.defante1@gmail.com>
2021-04-21 15:53:18 -0700

    updates

--- f1_pred/pred/union.sql
+++ f1_pred/pred/union.sql
@@ -1,10 +1,10 @@
 select 
 	m.*
 	,case when m.Compound is null then 'UNKNOWN' else m.Compound end as Tyre
-	,case when m.TrackStatus like '%1%' then 1 else 0 end as track_clear
-	,case when m.TrackStatus like '%2%' then 1 else 0 end as yellow_flag
-	,case when m.TrackStatus like '%4%' then 1 else 0 end as safety_car
-	,case when m.TrackStatus like '%5%' then 1 else 0 end as red_flag
-	,case when m.TrackStatus like '%6%' then 1 else 0 end as vsc_deployed
-	,case when m.TrackStatus like '%7%' then 1 else 0 end as vsc_ending
+	,case when m.TrackStatus like '%1%' then 'yes' else 'no' end as track_clear
+	,case when m.TrackStatus like '%2%' then 'yes' else 'no' end as yellow_flag
+	,case when m.TrackStatus like '%4%' then 'yes' else 'no' end as safety_car
+	,case when m.TrackStatus like '%5%' then 'yes' else 'no' end as red_flag
+	,case when m.TrackStatus like '%6%' then 'yes' else 'no' end as vsc_deployed
+	,case when m.TrackStatus like '%7%' then 'yes' else 'no' end as vsc_ending
 from '2021_gp' m
