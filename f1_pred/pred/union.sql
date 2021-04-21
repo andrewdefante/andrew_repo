6420845b6a5d1cb2b8820d6e6bef3e24b8c28921
Andrew Defante <andrew.defante1@gmail.com>
2021-04-20 17:28:03 -0700

    From the dashboard I can gather information regarding practice plans and general pace by imposing the built in features on the side panel.

--- f1_pred/pred/union.sql
+++ f1_pred/pred/union.sql
@@ -1,21 +1,10 @@
 select 
-b.*
-,'Bahrain Grand Prix' as gp
-,case when TrackStatus like '%1%' then 'Yes' else 'No' end as track_clear
-,case when TrackStatus like '%2%' then 'Yes' else 'No' end as yellow_flag
-,case when TrackStatus like '%4%' then 'Yes' else 'No' end as safety_car
-,case when TrackStatus like '%5%' then 'Yes' else 'No' end as red_flag
-,case when TrackStatus like '%6%' then 'Yes' else 'No' end as vsc_deployed
-,case when TrackStatus like '%7%' then 'Yes' else 'No' end as vsc_end
-from bahrain_21_all b
-union
-select 
-i.*
-,'Imola Grand Prix' as gp
-,case when TrackStatus like '%1%' then 'Yes' else 'No' end as track_clear
-,case when TrackStatus like '%2%' then 'Yes' else 'No' end as yellow_flag
-,case when TrackStatus like '%4%' then 'Yes' else 'No' end as safety_car
-,case when TrackStatus like '%5%' then 'Yes' else 'No' end as red_flag
-,case when TrackStatus like '%6%' then 'Yes' else 'No' end as vsc_deployed
-,case when TrackStatus like '%7%' then 'Yes' else 'No' end as vsc_end
-from imola_all i
+	m.*
+	,case when m.Compound is null then 'UNKNOWN' else m.Compound end as Tyre
+	,case when m.TrackStatus like '%1%' then 1 else 0 end as track_clear
+	,case when m.TrackStatus like '%2%' then 1 else 0 end as yellow_flag
+	,case when m.TrackStatus like '%4%' then 1 else 0 end as safety_car
+	,case when m.TrackStatus like '%5%' then 1 else 0 end as red_flag
+	,case when m.TrackStatus like '%6%' then 1 else 0 end as vsc_deployed
+	,case when m.TrackStatus like '%7%' then 1 else 0 end as vsc_ending
+from '2021_gp' m
